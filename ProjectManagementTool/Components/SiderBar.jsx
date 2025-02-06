e83805d8be6d08a3931e3753cd3b8619804786e0
=Manjil <aryalmanzeel1@gmail.com>
2025-02-06 22:46:02 +0545

    Adding options

--- ProjectManagementTool/Components/SiderBar.jsx
+++ ProjectManagementTool/Components/SiderBar.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const SiderBar = () => {
   return (
     <div className='fixed w-[200px] flex flex-col h-full gap-3 border-red-700 border-2 bg-white'>
-        <p>This is option</p>
+        <ul>
+          <p>Home</p>
+          <p>Teams</p>
+          <p>Assignment</p>
+          <p></p>
+          <p></p>
+        </ul>
       
     </div>
   )
