7c1946bb9496a17e5b3f4e675763663b03e1e89f
=Manjil <aryalmanzeel1@gmail.com>
2025-08-04 20:34:40 +0545

    doing

---
+++ 30 Days learning challenge/Day 13/script.js
@@ -0,0 +1,28 @@
+// const person = {
+//   firstName: "John",
+//   lastName : "Doe",
+//   id       : 5566,
+//   fullName : function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// };
+
+// fullName : function() {
+//   return this.firstName + " " + this.lastName;
+// }
+// let x = this;
+
+// class Car {
+//   constructor(name, year) {
+//     this.name = name;
+//     this.year = year;
+//   }
+// // }
+
+// const message = () => {
+// const name = "Jesse";
+// const age = 40;
+// return name + ' is ' + age + 'years old.';
+// };
+
+// export default message;
